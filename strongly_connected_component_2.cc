1c10f9b08c2d21d6d1676ea32bee9828d604e10b
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-08-13 23:23:59 +0900

    Create strongly_connected_component_2.cc

---
+++ strongly_connected_component_2.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include"../lib/strongly_connected_component.h"
+
+int main() {
+  int n,m; cin >> n >> m;
+  vector<vector<int>> adj(n);
+  for(int i=0;i<m;++i) {
+    int a,b; cin >> a >> b;
+    adj[a].push_back(b);
+  }
+  StronglyConnectedComponent scc(adj);
+  cout << scc.groups.size() << '\n';
+  for(auto& g : scc.groups) {
+    int sz = g.size();
+    cout << sz;
+    for(int i=0; i<sz; ++i) cout << ' ' << g[i];
+    cout << '\n';
+  }
+}
