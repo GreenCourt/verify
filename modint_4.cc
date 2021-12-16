c9c785191be9d0bf9b5ee36f7d528683f61274a5
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-12-16 21:01:39 +0900

    Update modint_4.cc

--- modint_4.cc
+++ modint_4.cc
@@ -6,14 +6,14 @@ using namespace std;
 int main() {
   // test mod_combination
   int n=1000;
-  mod_combination mc(n);
-  for( int i=1; i<=n; i++) for( int j=1; j<=i; j++) {
-    if(i%100==0 && j==1) cerr << i << endl;
-    modint c1 = mc.choose(i,j);
-    modint c2 = choose(i,j);
+  ModBinomial mb(n);
+  for( int i=0; i<=n; i++) for( int j=0; j<=i; j++) {
+    if(i%100==0 && j==i) cerr << i << endl;
+    modint c1 = mb.choose(i,j);
+    modint c2 = modint::choose(i,j);
     assert(c1==c2);
-    modint p1 = mc.permutation(i,j);
-    modint p2 = permutation(i,j);
+    modint p1 = mb.permutation(i,j);
+    modint p2 = modint::permutation(i,j);
     assert(p1==p2);
   }
 }
