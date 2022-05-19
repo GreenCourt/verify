9b7fea0b7976cc6abcfc7698c507f3d5fe95a940
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-05-19 17:52:12 +0900

    Update lazy_segment_tree_2.cc

--- lazy_segment_tree_2.cc
+++ lazy_segment_tree_2.cc
@@ -6,19 +6,16 @@ using namespace std;
 // https://atcoder.jp/contests/practice2/tasks/practice2_l
 
 /* Range inversion number, Range Xor */
-struct S{ int x[2]; long long rev; int size; };
+struct S{ int x[2]; long long rev; };
 using F = int;
-S op(S a, S b){ return {a.x[0]+b.x[0], a.x[1]+b.x[1], (long long)a.x[1] * b.x[0] + a.rev + b.rev, a.size+b.size}; }
-S e(){ return {0, 0, 0, 0}; }
+S op(S a, S b){ return {a.x[0]+b.x[0], a.x[1]+b.x[1], (long long)a.x[1] * b.x[0] + a.rev + b.rev}; }
+S e(){ return {0, 0, 0}; }
 S mapping(F f, S x){
   if(f) {
-    long long comb_all = (long long)x.size * (x.size-1) / 2;
-    long long comb_ones = (long long)x.x[0] * (x.x[0]-1) / 2;
-    long long comb_zeros = (long long)x.x[1] * (x.x[1]-1) / 2;
-    return {x.x[1], x.x[0], comb_all - comb_ones - comb_zeros - x.rev, x.size};
+    return {x.x[1], x.x[0], (long long)x.x[0] * x.x[1] - x.rev};
   }
   else {
-    return  {x.x[0], x.x[1], x.rev, x.size};
+    return  {x.x[0], x.x[1], x.rev};
   }
 }
 F composition(F f, F g){ return f ^ g; }
@@ -27,7 +24,7 @@ F id(){ return 0; }
 int main() {
   int n, q;
   cin >> n >> q;
-  vector<S> initial(n, {0,0,0,1});
+  vector<S> initial(n, {0,0,0});
   for(int i=0; i<n; ++i) {
     int a; cin >> a;
     initial[i].x[a] = 1;
