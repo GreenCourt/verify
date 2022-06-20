d637dbc7f8307dcac1337630dfe96928247a6213
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-06-20 22:14:11 +0900

    Update integer_math_3.cc

--- integer_math_3.cc
+++ integer_math_3.cc
@@ -6,16 +6,16 @@ using namespace std;
 int main() {
   // https://atcoder.jp/contests/abc191/tasks/abc191_d
   double x,y,r; cin >> x >> y >> r;
-  long long left  = ceil(x-r);
-  long long right = floor(x+r);
+  long long left  = llround(ceil(x-r));
+  long long right = llround(floor(x+r));
   long long ans = 0;
   const int base = 10000;
-  long long rr = round(r*base)*round(r*base);
-  for(int i=left; i<=right; i++) {
+  long long rr = llround(r*base) * llround(r*base);
+  for(long long i=left; i<=right; i++) {
     double t = x-(double)i;
-    long long tt = round(t*base)*round(t*base);
+    long long tt = llround(t*base) * llround(t*base);
     long long p = isqrt(rr-tt);
-    long long Y = round(y*base);
+    long long Y = llround(y*base);
     long long u = iceil(Y-p, base) / base;
     long long v = ifloor(Y+p, base) / base;
     ans += v - u + 1;
